6d882f1281a16a2caaebf19c256b2ce3deaf133b
Alexander Nuykin <scepticnav@gmail.com>
2022-07-03 19:49:16 +0300

    Add code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] InputArray(string[] newArray)
+{
+    Console.WriteLine("Введите количество элементов массива:");
+    int elementsCount = int.Parse(Console.ReadLine());
+    string[] createArray = new string[elementsCount];
+    for (int i = 0; i < createArray.Length; i++)
+    {
+        Console.Write($"Введите элемент массива под индексом {i}: ");
+        createArray[i] = Console.ReadLine();
+    }
+    return createArray;
+}
+
+string PrintArray(string[] array)
+{
+    string res = String.Empty;
+    int len = array.Length;
+    for (int i = 0; i < len; i++)
+    {
+        res += $"{array[i]} ";
+    }
+    return res;
+}
+
+string[] OutputArray(string[] inputArray)
+{
+    Console.WriteLine("Введите максимальное число символов элементов массива, которые будут отображаться:");
+    int limit = int.Parse(Console.ReadLine());
+    int newLen = 0;
+    for (int k = 0; k < inputArray.Length; k++)
+    {
+        if (inputArray[k].Length <= limit) newLen++;
+    }
+
+    string[] outputArray = new string[newLen];
+    int i, index;
+    index = i = 0;
+
+    while (i < inputArray.Length)
+    {
+        if (inputArray[i].Length <= limit)
+        {
+            outputArray[index] = inputArray[i];
+            index++;
+        }
+        i++;
+    }
+
+    return outputArray;
+}
+
+string[] emptyArray = new string[0];
+var inArray = InputArray(emptyArray);
+Console.WriteLine();
+Console.WriteLine($"Исходный массив, созданный Вами:  {PrintArray(inArray)}");
+var outArray = OutputArray(inArray);
+Console.WriteLine($"Отформатированный массив:  {PrintArray(outArray)}");
